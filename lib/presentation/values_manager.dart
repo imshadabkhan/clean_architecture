f59d59ce9c1fb38409f7ac0d4f81a343849e4c02
Shadab khan <117011709+imshadabkhan@users.noreply.github.com>
2024-08-03 04:01:21 -0700

    app values added

---
+++ lib/presentation/values_manager.dart
@@ -0,0 +1,35 @@
+class AppMargin{
+  static const double m8=8.0;
+  static const double m10=10.0;
+  static const double m12=12.0;
+  static const double m14=14.0;
+  static const double m16=16.0;
+  static const double m18=18.0;
+  static const double m20=20.0;
+}
+
+class AppPadding{
+  static const double m8=8.0;
+  static const double m10=10.0;
+  static const double m12=12.0;
+  static const double m14=14.0;
+  static const double m16=16.0;
+  static const double m18=18.0;
+  static const double m20=20.0;
+}
+
+class AppSize{
+  static const double m8=8.0;
+  static const double m10=10.0;
+  static const double m12=12.0;
+  static const double m14=14.0;
+  static const double m16=16.0;
+  static const double m18=18.0;
+  static const double m20=20.0;
+  static const double m28=28.0;
+  static const double m35=35.0;
+  static const double m45=45.0;
+  static const double m60=60.0;
+  static const double m70=70.0;
+}
+
